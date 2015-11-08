84b875787e9584bf015524e90cd032b61181d651
alprusa <aprusa@ucsc.edu>
2015-11-08 02:24:53 -0800

    Finished models started making player actions

---
+++ CatWalkersExtremeDigPrototype/Assets/Scripts/Cat1Controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat1Controller : MonoBehaviour {
+	
+	public Rigidbody rigidBody;
+	public Boundary boundary;
+
+	public float tilt;
+	public float speedVert;
+	public float speedHoriz;
+
+	void Start(){
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate(){
+		float horizontalMovement =  Input.GetAxis("Horizontal");
+		float verticalMovement =  Input.GetAxis("Vertical");
+
+		Vector3 vel = new Vector3(horizontalMovement*speedHoriz, 0.0f, verticalMovement*speedVert);
+		rigidBody.velocity = vel;
+		rigidBody.position = new Vector3 (
+			Mathf.Clamp(rigidBody.position.x, boundary.xMin, boundary.xMax),
+			0.69f,
+			Mathf.Clamp(rigidBody.position.z, boundary.zMin, boundary.zMax)
+		);
+	}
+}
---
+++ CatWalkersExtremeDigPrototype/Assets/Scripts/DestroyBoundary.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBoundary : MonoBehaviour {
+
+	void OnTriggerExit(Collider obj){
+		Destroy (obj.gameObject);
+	}
+
+	//create a restart respawn function so that the game doesn't just end cause you've left the field
+}
